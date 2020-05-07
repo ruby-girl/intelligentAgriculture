97e84011c7ba23f71b2677835b1cfa7861127798
Administrator <1017390115@qq.com>
2020-05-07 15:40:35 +0800

    request

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 {}
 import Vue from 'vue'
 import App from './App'
-import {
-	request
-} from '@/utils/request.js'
+
+import * as api from '@/common/api.js'
 import validate from '@/utils/graceChecker.js'
 
 import store from './store'
-   import xflSelect from './components/xfl-select/xfl-select.vue';
-   import uniPopup from './components/uni-popup/uni-popup.vue';
+import xflSelect from './components/xfl-select/xfl-select.vue';
+import uniPopup from './components/uni-popup/uni-popup.vue';
 Vue.component('xfl-select', xflSelect);
 Vue.component('uni-popup', uniPopup);
 import cuCustom from './colorui/components/cu-custom.vue'
-Vue.component('cu-custom',cuCustom)
-import amap from '@/utils/amap-wx.js';  
-
+Vue.component('cu-custom', cuCustom)
+import amap from '@/utils/amap-wx.js';
+Vue.prototype.$api = api;
 Vue.prototype.$validate = validate
 Vue.prototype.amap = amap
 Vue.config.productionTip = false
 Vue.prototype.$store = store
 App.mpType = 'app'
-Vue.prototype.$http = request
+
 const app = new Vue({
 	store,
 	...App
