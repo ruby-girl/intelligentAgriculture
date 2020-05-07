5ab45c2028bf18d252efb86396538e7e636b5cf0
Administrator <1017390115@qq.com>
2020-05-07 10:35:05 +0800

    测试

---
+++ pages/plantManage/chooseHandle.vue
@@ -0,0 +1,53 @@
+<!-- 选择加入基地  or 创建基地 -->
+<template>
+	<view class="choose-land tc">
+		
+		
+			<image src="/static/plant/pic_nodata@2x.png" class="null-img"></image>
+
+		
+		<view class="cr3 f20 "> 暂无数据</view>
+		<view>只有创建或者加入基地（合作社或者个人），<br/>
+			才能进行进一步的种植管理。</view>
+			<button class="cu-btn block bg-green  lg">创建基地</button>
+			<button class="cu-btn block   lg line-green">加入基地</button>
+			
+
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.tc {
+		text-align: center;
+	}
+
+	.choose-land {
+		background-color: #fff;
+		padding: 30rpx;
+		height: calc(100vh - 70rpx) ;
+		overflow: hidden;
+	.cu-btn{
+			
+			margin-top: 50rpx;
+		
+		}
+		
+
+		.null-img {
+			height: 293rpx;
+			width: 243rpx;
+
+		}
+	}
+</style>
