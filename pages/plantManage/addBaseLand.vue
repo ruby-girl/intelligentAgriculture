879daf3830fbecc137d66f7d45c94ecb68f4a3d4
Administrator <1017390115@qq.com>
2020-05-12 10:03:41 +0800

    land

--- pages/plantManage/addBaseLand.vue
+++ pages/plantManage/addBaseLand.vue
@@ -1,40 +1,8 @@
 <!-- 创建基地 -->
 <template>
-	<web-view src="http://192.168.101.32:8081/#/"></web-view>
+	<web-view src="http://192.168.101.32:8080/#/"></web-view>
 
-	<!-- <view class="add-base-land"> 
-		<map-item v-on:dataMsg="getMsg" ref="childMap"></map-item>
-		<view class="draw-content showModel" :class="{'showModel':!isEdit}">
-			<view class="tr"> 
-				<button class="cu-btn  line-green" style="margin-right: 10px;" @click="clearMap">清空</button>
-				<button class="cu-btn  bg-green  " @click="handleMapStart" v-if="isEdit == 1"> 开始圈地</button>
-				<button class="cu-btn  bg-green  " @click="handleMapEnd" v-if="isEdit == 2"> 结束圈地</button>
-				<button class="cu-btn  bg-green  " @click="handleMapEdit" v-if="isEdit == 3"> 编辑圈地</button>
-			</view>
-			<view class="gutter-blank"></view>
-			<view style="padding: 30rpx; box-sizing: content-box;">
-				<text class="f20">填写基地信息</text>
-				<view class="cu-form-group ">
-					<view class="title">
-						基地名称
-					</view>
-					<input placeholder="基地名称" name="input"></input>
-				</view>
-				<view class="cu-form-group">
-					<view class="title">基地面积</view>
-					<input placeholder="基地面积" name="input"></input>
-				</view>
-				<view class="cu-form-group">
-					<view class="title">基地地址</view>
-					<picker mode="multiSelector" :value="multiIndex" :range="multiArray">
-						<view class="picker">
-							{{multiArray[0][multiIndex[0]]}}，{{multiArray[1][multiIndex[1]]}}，{{multiArray[2][multiIndex[2]]}}
-						</view>
-					</picker>
-				</view>
-				<button class="cu-btn  bg-green  lg block">创建基地</button>
-			</view>
-		</view> -->
+
 	</view>
 </template>
 <script>
@@ -120,19 +88,7 @@
 		transition: height .25s;
 		z-index: 999;
 
-		.tr {
-			padding: 20rpx;
-			text-align: right;
-		}
-
-		.gutter-blank {
-			width: 100vw;
-			height: 20rpx;
-			background-color: #F2F2F2;
-		}
 	}
 
-	.showModel {
-		height: 48vh;
-	}
+	
 </style>
