5884e504209414baf4a75d75a380483fa7ca230b
Administrator <1017390115@qq.com>
2020-05-15 14:34:50 +0800

    add农事

--- pages/plantManage/framManage/formModel.vue
+++ pages/plantManage/framManage/formModel.vue
@@ -2,221 +2,356 @@
 	<view class="add-fram">
 		<view class="section">
 			<view class="label display-flex  justify-content-flex-justify">
-				<label><text class="line"></text><text class="fb">人工（0）数组 </text></label>
+				<label><text class="line"></text><text class="fb">人工（{{personList.length}}） </text></label>
 				<view class="display-flex">
-					<view style="color: red;margin-right: 8px;">
-						<image src="/static/plant/icon_del@2x.png" class="imgIcon"></image> 删除
-					</view>
+
 					<view class="cr">
 						<image src="/static/plant/icon_add@2x.png" class="imgIcon" @click="personAdd"></image> 添加
 					</view>
 				</view>
-			</view> 
-			<view v-for="(item,index) in personList" :key='index'class="item-border">
-				
-		
-			<view class="cu-form-group ">
-				<view class="title"> 人员类型 </view>
-				<input placeholder="请输入" @input="changeInput($event,'name')" name="input"></input>
 			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-					人工（亩）
+			<view v-for="(item,index) in personList" :key='index' class="item-border">
+
+				<view class="display-flex justify-content-flex-justify item-title">
+					<view>
+						{{index+1}}.人工
+					</view>
+					<veiw>
+						<text class="iconfont iconquxiao imgIcon " @click="personDelete('personList',index)"></text>
+					</veiw>
 				</view>
-				<input placeholder="请输入" @input="changeInput($event,'labour')" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
+				<view class="cu-form-group ">
+					<view class="title"> 人员类型 </view>
+					<input placeholder="请输入" @input="changeInput($event,'name',index)" name="input"></input>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+						人工（亩）
+					</view>
+					<input placeholder="请输入" type="digit" @input="changeInput($event,'labour',index)" name="input"></input>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
 
-					工价（元）
+						工价（元）
+
+					</view>
+					<input placeholder="请输入" type="digit" @input="changeInput($event,'labourCost',index)" name="input"></input>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+
+						面积（亩）
+
+					</view>
+					<input placeholder="请输入" disabled  name="input"></input>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+
+						费用合计
+
+					</view>
+					<input placeholder="请输入" disabled  name="input"></input>
+				</view>
+			</view>
+
+		</view>
+		<view class="section">
+			<view class="label display-flex  justify-content-flex-justify">
+				<label><text class="line"></text><text class="fb">农资（{{suppliesList.length}}） </text></label>
+				<view class="display-flex">
+
+					<view class="cr">
+						<image src="/static/plant/icon_add@2x.png" class="imgIcon" @click="suppliesAdd"></image> 添加
+					</view>
+				</view>
+			</view>
+			<view v-for="(item,index) in suppliesList" :key='index' class="item-border">
+				<view class="display-flex justify-content-flex-justify item-title">
+					<view>
+						{{index+1}}.农资
+					</view>
+					<veiw>
+
+						<text class="iconfont iconquxiao imgIcon " @click="personDelete('suppliesList',index)"></text>
+
+					</veiw>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+						资料名称
+					</view>
+					<input placeholder="请输入" @input="changeInput2($event,'name',index)" name="input"></input>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+
+						生产厂家
+
+					</view>
+					<picker @change="companyChange($event,'supplierName',index)" range-key="name" :value="companySelval" :range="companySelectList">
+						<view class="picker">
+							{{companySelval?companySelectList[companySelval].name:'请选择'}}
+
+
+						</view>
+					</picker>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+
+						单位
+
+					</view>
+					<picker @change="unitsChange($event,'unit',index)" range-key="name" :value="unitSelval" :range="unitSelectList">
+						<view class="picker">
+							{{unitSelval?unitSelectList[unitSelval].name:'请选择'}}
+
+
+						</view>
+					</picker>
 
 				</view>
-				<input placeholder="请输入" @input="changeInput($event,'labourCost')" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
+				<view class="cu-form-group ">
+					<view class="title">
 
-					面积（亩）
+						每亩用量
 
-				</view>
-				<input placeholder="请输入" @input="changeInput($event,'acreageCount')" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
+					</view>
 
-					费用合计
+					<input placeholder="请输入" @input="changeInput2($event,'suppliesCount',index)" name="input"></input>
 
 				</view>
-				<input placeholder="请输入" @input="changeInput($event,'personFeeCount')" name="input"></input>
-			</view>
+				<view class="cu-form-group ">
+					<view class="title">
+
+
+						单价（元）
+
+
+					</view>
+					<input placeholder="请输入" @input="changeInput2($event,'price',index)" name="input"></input>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+
+
+						面积（亩）
+
+
+					</view>
+					<input placeholder="请输入" @input="changeInput2($event,'acreageCount',index)" name="input"></input>
+				</view>
+				<view class="cu-form-group ">
+					<view class="title">
+
+
+
+						费用
+
+
+
+					</view>
+					<input placeholder="请输入" @input="changeInput2($event,'suppliesFeeCount',index)" name="input"></input>
+				</view>
+
 			</view>
 		</view>
 		<view class="section">
 			<view class="label display-flex  justify-content-flex-justify">
-				<label><text class="line"></text><text class="fb">农资（0）数组 </text></label>
+				<label><text class="line"></text><text class="fb">设备（{{equitmenList.length}}） </text></label>
 				<view class="display-flex">
-					<view style="color: red;margin-right: 8px;">
-						<image src="/static/plant/icon_del@2x.png" class="imgIcon"></image> 删除
-					</view>
+
 					<view class="cr">
-						<image src="/static/plant/icon_add@2x.png" class="imgIcon"></image> 添加
+						<image src="/static/plant/icon_add@2x.png" class="imgIcon" @click="equitmenAdd"></image> 添加
 					</view>
 				</view>
 			</view>
-
-			<view class="cu-form-group ">
-				<view class="title">
-					资料名称
-				</view>
-				<picker @change="PickerChange" :value="ttt" :range="picker">
-					<view class="picker">
-
-						{{ttt>-1?picker[ttt]:'请选择'}}
-
+			<view v-for="(item,index) in equitmenList" :key='index' class="item-border">
+				<view class="display-flex justify-content-flex-justify item-title">
+					<view>
+						{{index+1}}.设备
 					</view>
-				</picker>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-
-					生产厂家
-
+					<veiw>
+						<text class="iconfont iconquxiao imgIcon " @click="personDelete('equitmenList',index)"></text>
+					</veiw>
 				</view>
-				<picker @change="PickerChange" :value="ttt" :range="picker">
-					<view class="picker">
-
-						{{ttt>-1?picker[ttt]:'请选择'}}
-
+				<view class="cu-form-group ">
+					<view class="title">
+						设备名称
 					</view>
-				</picker>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-
-					每亩用量
-
+					<input placeholder="请输入" @input="changeInput3($event,'name',index)" name="input"></input>
 				</view>
-				<input placeholder="请输入" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-
-
-					单价（元）
-
-
+				<view class="cu-form-group ">
+					<view class="title">
+						每亩收费（元）
+					</view>
+					<input placeholder="请输入" @input="changeInput3($event,'price',index)" name="input"></input>
 				</view>
-				<input placeholder="请输入" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-
-
-					面积（亩）
-
-
+				<view class="cu-form-group ">
+					<view class="title">
+						面积（亩）
+					</view>
+					<input placeholder="请输入" @input="changeInput3($event,'acreageCount',index)" name="input"></input>
 				</view>
-				<input placeholder="请输入" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-
-
-
-					费用
-
-
-
+				<view class="cu-form-group ">
+					<view class="title">
+						费用
+					</view>
+					<input placeholder="请输入" @input="changeInput3($event,'feeCount',index)" name="input"></input>
 				</view>
-				<input placeholder="请输入" name="input"></input>
 			</view>
+
 		</view>
-		<view class="section">
-			<view class="label display-flex  justify-content-flex-justify">
-				<label><text class="line"></text><text class="fb">设备（0）数组 </text></label>
-				<view class="display-flex">
-					<view style="color: red;margin-right: 8px;">
-						<image src="/static/plant/icon_del@2x.png" class="imgIcon"></image> 删除
-					</view>
-					<view class="cr">
-						<image src="/static/plant/icon_add@2x.png" class="imgIcon"></image> 添加
-					</view>
-				</view>
-			</view>
+		<button class="cu-btn block bg-green  lg" @click="saveForm"> 提交 </button>
+		<neil-modal :show="showModel" title="提示" content="是否删除？" @cancel="bindBtn" @confirm="sureDeleteFun">
+		</neil-modal>
 
-			<view class="cu-form-group ">
-				<view class="title">
-					设备名称
-				</view>
-				<picker @change="PickerChange" :value="ttt" :range="picker">
-					<view class="picker">
-
-						{{ttt>-1?picker[ttt]:'请选择'}}
-
-					</view>
-				</picker>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-					每亩收费（元）
-				</view>
-				<input placeholder="请输入" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-					面积（亩）
-				</view>
-				<input placeholder="请输入" name="input"></input>
-			</view>
-			<view class="cu-form-group ">
-				<view class="title">
-					费用
-				</view>
-				<input placeholder="请输入" name="input"></input>
-			</view>
-		</view>
-		<button class="cu-btn block bg-green  lg">提交</button>
 	</view>
+
 </template>
 
 <script>
+	import neilModal from '@/components/neil-modal/neil-modal.vue';
+
 	export default {
+		components: {
+			neilModal
+		},
 		data() {
 			return {
-				personList: [{
-					"name": "",
-					"labour": 0,
-					"labourCost": 0,
-					"acreageCount": 0,
-					"personFeeCount": 0,
+				ttt: '',
+				showModel: false,
+				picker: [],
+				personList: [],
+				
+				nowDeletePersonIndex: '',
+				nowName: '',
 
-					"id": 0,
+				suppliesList: [],
+				companySelval: '',
+				companySelectList: [],
+				unitSelval: '',
+				unitSelectList: [],
 
+				equitmenList: []
 
-					"warmWorkRecordId": 0,
-					"workOrderId": 0
-				}]
 			};
 		},
+		created() {
+			this.initSelect()
+		},
 		methods: {
+			initSelect() {
+				this.$api.getSuppliersCompany().then(res => {
+					this.companySelectList = res.data.data
+				})
+				this.$api.getSuppliersUnits().then(res => {
+					this.unitSelectList = res.data.data
+				})
+			},
+			saveForm() {
+				this.$api.addPersonResources(this.personList).then(res => {})
+				this.$api.addSuppliessResources(this.suppliesList).then(res => {})
+				this.$api.addEquitmenResources(this.equitmenList).then(res => {});
+				wx.showToast({
+					title: '成功',
+					icon: 'success',
+					duration: 2000
+				});
+				wx.navigateTo({
+					url: '../plantManage/framManage/framManage'
+				})
+
+
+			},
 			/* 人工 */
 			personAdd() {
 				let obj = {
 					"name": "",
-					"labour": 0,
-					"labourCost": 0,
-					"acreageCount": 0,
-					"personFeeCount": 0,					
-					"id": 0,
-					"warmWorkRecordId": 0,
-					"workOrderId": 0
+					"labour": '',
+					"labourCost": '',
+					"acreageCount": '',
+					"personFeeCount": '',
+					"farmWorkRecordId": 1,
+					"workOrderId": 3
 				}
 				this.personList.push(obj)
+			},
+			/* 删除人工 */
+			personDelete(name,index) {
+				this.showModel = true;
+				this.nowName = name;
+				this.nowDeletePersonIndex = index;
+			},
+			bindBtn() {
+				this.showModel = false;
+			},
+			sureDeleteFun() {
+				this.showModel = false;
+				this[this.nowName].splice(this.nowDeletePersonIndex, 1)
+			},
+			/* 农资 */
+			suppliesAdd() {
+				let obj = {
+					"name": "",
+					"supplierName": "",
+					"unit": "",
+					"suppliesCount": "",
+					"price": "",
+					"acreageCount": "",
+					"suppliesFeeCount": "",
+					"farmWorkRecordId": 1,
+					"workOrderId": 3
+				}
+				this.suppliesList.push(obj)
 
 			},
-			changeInput(e, d) {
+			companyChange(e, n, i) {
+				this.companySelval = e.detail.value
+				this.suppliesList[i][n] = this.companySelectList[e.detail.value].name
+			},
+			unitsChange(e, n, i) {
+				this.unitSelval = e.detail.value
+				this.suppliesList[i][n] = this.unitSelectList[e.detail.value].name
+			},
+			/* 设备 */
+			equitmenAdd() {
+				let obj = {
+					"name": "",
+					"price": "",
+					"acreageCount": "",
+					"feeCount": "",
+					"farmWorkRecordId": 1,
+					"workOrderId": 3
+				}
+				this.equitmenList.push(obj)
+			},
 
-			}
+			/* 人工输入框数据 */
+			changeInput(e, v, index) {
+				if(v == 'labour'){
+					
+				}
+				if(v == 'labourCost'){
+					
+				}
+				
+				this.personList[index][v] = e.detail.value;
+
+			},
+			/* 农资  输入框数据 */
+			changeInput2(e, v, index) {
+				this.suppliesList[index][v] = e.detail.value;
+			},
+			/* 设备  输入框数据 */
+			changeInput3(e, v, index) {
+				this.equitmenList[index][v] = e.detail.value;
+			},
+
+
+
 		}
 	}
 </script>
@@ -231,13 +366,14 @@
 		}
 
 		.imgIcon {
+			display: inline-block;
 			width: 40rpx;
 			height: 40rpx;
 			vertical-align: text-bottom;
+			color: #333333;
 		}
 
 		.section {
-			background-color: #fff;
 			padding: 30rpx;
 			margin-bottom: 30rpx;
 		}
@@ -248,9 +384,6 @@
 
 		.label {
 
-
-
-			border-bottom: 1px solid #E1E1E1;
 			padding-bottom: 16rpx;
 			margin-bottom: 20rpx;
 
@@ -267,8 +400,18 @@
 				vertical-align: text-top;
 			}
 		}
-		.item-border{
-			border-bottom: 1px solid red;
+
+		.item-border {
+			background: rgba(255, 255, 255, 1);
+			box-shadow: 0px 3px 7px 0px rgba(0, 0, 0, 0.1);
+			border-radius: 4px;
+			margin-bottom: 30rpx;
+
+			.item-title {
+				background-color: #EEFEF0;
+				padding: 20rpx 30rpx;
+			}
+
 		}
 	}
 </style>
