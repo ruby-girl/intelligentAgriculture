5ebbdbd29aae16305dfaa18861c6b5e0cb7e820f
Administrator <1017390115@qq.com>
2020-05-20 14:00:09 +0800

    修改

--- pages/plantManage/framManage/formModel.vue
+++ pages/plantManage/framManage/formModel.vue
@@ -327,16 +327,10 @@ this.$nextTick(function() {
 						title: '提交成功',
 						icon: 'success',
 						success() {
-							uni.navigateBack({
-								delta: 1,
-								success() {
-									let page = getCurrentPages().pop(); //跳转页面成功之后
-									if (!page) return;
-									page.onLoad({
-										id: obj.workOrderId
-									}); //如果页面存在，则重新刷新页面
-								}
-							})
+							uni.navigateTo({
+							    url: '/pages/plantManage/workOrder?type=2&baseId='+uni.getStorageSync('baseId')
+							});
+							
 
 						}
 					})
