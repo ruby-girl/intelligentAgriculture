0912d55054f13e1c8ba1a3b1730b4521841b64fe
maxois <1014820110@qq.com>
2020-09-02 16:40:59 +0800

    解决我的农场冲突

--- pages/personal/detailFarm.vue
+++ pages/personal/detailFarm.vue
@@ -32,12 +32,17 @@
 					</map>
 					<view class="map-top-box flex align-items-center justify-content-flex-justify">
 						<view class="detail-name">
+							<view>
+								<text style="font-weight: bold;font-size: 16px;">{{ farmDetail.farmName }}</text>
+							</view>
 							<view class="detail-small-txt">
 								<image style="width: 17px;height: 20px;" src="../../static/imgs/location-2.png" mode="aspectFill"></image>
+								{{ farmAddress }}
 							</view>
 						</view>
 						<view class="detail-img" @click="editFarm">
 							<image src="../../static/imgs/deit.png" mode="aspectFill"></image>
+							<view>编辑</view>
 						</view>
 					</view>
 					<view class="map-bottom-box">
@@ -45,23 +50,49 @@
 						<view class="flex align-items-center justify-content-flex-justify">
 							<view class="detail-name flex align-items-center" style="border:none;">
 								<!-- <image style="width: 30px;height: 30px;" src="../../static/imgs/deit.png" mode="aspectFill"></image> -->
+								<view class="cu-avatar lg round" :style="{ backgroundImage: 'url(' + imgUrl + farmDetail.masterPicture + ')' }"></view>
 								<view style="line-height:18px;margin-right: 5px;">
+									<view>{{ farmDetail.master }}</view>
+									<view class="detail-small-txt">{{ farmDetail.fphone }}</view>
 								</view>
 							</view>
 							<view class="detail-img">
+								<image @click="callPhone(farmDetail.fphone)" style="width: 30px;height: 30px;" src="../../static/imgs/phone.png" mode="aspectFill"></image>
 							</view>
 						</view>
 					</view>
 				</view>
 				<view class="farm-detail-box">
 					<view><text style="font-size: 16px;">农场介绍</text></view>
+					<view class="detail-small-txt">{{ farmDetail.introduce || '' }}</view>
 					<view class="flex align-items-center imgs-box">
+						<view v-for="(item, i) in imgArr" :key="i" class="cu-avatar lg" :style="{ backgroundImage: 'url(' + imgUrl + item + ')' }"></view>
 					</view>
 				</view>
 			</scroll-view>
 		</view>
-			<view v-bind:style="{ height: windowHeight - 20 + 'px', padding: '10px 0',paddingTop:'200rpx' }" v-show="TabCur == 3">
-			<scroll-view scroll-x class="cu-nav"  style="padding: 0 30rpx;position: fixed;z-index: 1;top: 90rpx;background-color: #F0F0F0;height: 120rpx;">
+		<view v-show="TabCur == 2">
+			<scroll-view
+				v-bind:style="{ height: windowHeight + 'px' }"
+				class="list-container"
+				scroll-y="true"
+				refresher-enabled="true"
+				refresher-background="#eee"
+				@refresherpulling="onPulling"
+				@refresherrefresh="onRefresh"
+				@refresherrestore="onRestore"
+				@refresherabort="onAbort"
+				:refresher-triggered="triggered"
+				:refresher-threshold="100"
+				@scrolltoupper="scrolltoupper"
+				@scrolltolower="loadingData"
+			>
+				<view class="list-item" v-for="(item, index) in newsList" :key="index" @tap="toUrl(item)"><land-block :itemObject="item" /></view>
+				<view class="loading-more">{{ contentdown }}</view>
+			</scroll-view>
+		</view>
+		<view v-bind:style="{ height: windowHeight - 20 + 'px', padding: '10px 0', paddingTop: '200rpx' }" v-show="TabCur == 3">
+			<scroll-view scroll-x class="cu-nav" style="padding: 0 30rpx;position: fixed;z-index: 1;top: 90rpx;background-color: #F0F0F0;height: 120rpx;">
 				<view class="flex text-center" style="margin-top: 30rpx;">
 					<view
 						class="cu-items flex-sub"
@@ -75,7 +106,7 @@
 					</view>
 				</view>
 			</scroll-view>
-			<scroll-view v-bind:style="{ height: windowHeight - 105 + 'px', backgroundColor: '#f0f0f0' }" class="list-containers" scroll-y="true">
+			<scroll-view v-bind:style="{ height: windowHeight - 105 + 'px', backgroundColor: '#f0f0f0' }" class="list-container-1" scroll-y="true">
 				<view style="height: 100%;background-color: #FFFFFF;">
 					<template v-if="WarningTabCur == 1">
 						<view class="cu-timeline" v-if="pestsList.length > 0">
@@ -111,137 +142,177 @@
 					</template>
 				</view>
 			</scroll-view>
+			<popup :content="modelContent" align="center" :show="popupShow" :showCancel="false" confirmText="我知道了" @close="closePopup" />
 		</view>
 	</view>
 </template>
 
 <script>
-	import QQMapWX from '@/static/qqmap-wx-jssdk.min.js';
-	import {
-		throttle
-	} from "@/utils/index.js"
-	import landBlock from '@/components/landBlock.vue'
-	var qqmapsdk;
-	export default {
-		components: {
-			landBlock,
-		},
-		data() {
-			return {
-				latitude: 39.909,
-				longitude: 116.39742,
-				covers: [{
+import QQMapWX from '@/static/qqmap-wx-jssdk.min.js';
+import { throttle } from '@/utils/index.js';
+import landBlock from '@/components/landBlock.vue';
+var qqmapsdk;
+export default {
+	components: {
+		landBlock
+	},
+	data() {
+		return {
+			latitude: 39.909,
+			longitude: 116.39742,
+			covers: [
+				{
 					latitude: '',
 					longitude: ''
-
-				}],
-				farmDetail: {},
-				farmId: '',
-				farmAddress: '',
-				tabs: [{
-						id: 1,
-						name: '农场信息'
-					}, {
-						id: 2,
-						name: '地块信息'
-					},
-					{
-						id: 3,
-						name: '预警'
-					}
-				],
-				waningTabs: [
-					{
-						id: 1,
-						name: '病虫害'
-					},
-					{
-						id:2,
-						name: '农场'
-					}
-				],
-				imgUrl: getApp().globalData.imgUrl,
-				TabCur: 1,
-				WarningTabCur: 1,
-				newsList: [],
-				page: 1,
-				moreHeight: 30,
-				windowHeight: 300,
-				contentdown: '',
-				loadingType: 0,
-				triggered: false,
-				_freshing: false,
-				pestsList:[],
-				popupShow:false,
-				modelContent:'',
-				imgArr:[],
-				timeList: [],
-				popupShow: false,
-				modelContent: '',
-				imgArr: [],
-			};
-		},
-		onLoad() {		
-			let _this = this
-			uni.getStorage({
-				key: 'farmId',
-				success: function(res) {
-					_this.farmId = res.data
-					_this.getFarmDetail()
-					_this.getData()
-					_this.massifFindFarmId()
-					_this.massifFindFarmPests()
 				}
-			})
-		},
-		mounted() {
-			this.loadingData = throttle(this.loadingData, 2000);
-		},
-		methods: {
-			callouttapFunc: function(e) {
-				// uni.navigateTo({
-				// 	url: 'nearTheEquipment?latitude='+this.latitude+'&longitude='+this.longitude+'&farmId='+this.farmId
-				// })
-				uni.navigateTo({
-					url: 'nearTheEquipment?farmId='+this.farmId
-				})
-			},
-			callPhone(phone) {
-				if (!phone) return;
-				uni.makePhoneCall({
-					// 手机号
-					phoneNumber: phone
-				});
-			},
-			showModel(txt) {
-				this.modelContent = txt
-				this.popupShow = true
-			},
-			closePopup() {
-				this.popupShow = false
-			},
-			onPulling() {},
-			onRefresh() {
-				if (this._freshing) return;
-				this._freshing = true;
-				if (!this.triggered) { //界面下拉触发，triggered可能不是true，要设为true  
-					this.triggered = true;
+			],
+			farmDetail: {},
+			farmId: '',
+			farmAddress: '',
+			tabs: [
+				{
+					id: 1,
+					name: '农场信息'
+				},
+				{
+					id: 2,
+					name: '地块信息'
+				},
+				{
+					id: 3,
+					name: '预警'
 				}
-				let _this = this
-				setTimeout(() => {
-					this.triggered = false; //触发onRestore，并关闭刷新图标
-					this._freshing = false;
-					_this.initData()
-				}, 1000)
-			},
-			onRestore() {
-				this.triggered = false; // 需要重置
-				this._freshing = false
-			},
-			onAbort() {
+			],
+			waningTabs: [
+				{
+					id: 1,
+					name: '病虫害'
+				},
+				{
+					id: 2,
+					name: '农场'
+				}
+			],
+			imgUrl: getApp().globalData.imgUrl,
+			TabCur: 1,
+			WarningTabCur: 1,
+			newsList: [],
+			page: 1,
+			moreHeight: 30,
+			windowHeight: 300,
+			contentdown: '',
+			loadingType: 0,
+			triggered: false,
+			_freshing: false,
+			timeList: [],
+			pestsList: [],
+			popupShow: false,
+			modelContent: '',
+			imgArr: [],
+			timeList: [],
+			popupShow: false,
+			modelContent: '',
+			imgArr: []
+		};
+	},
+	onLoad(option) {
+		this.windowHeight = uni.getSystemInfoSync().windowHeight; // 屏幕的高度
+		qqmapsdk = new QQMapWX({
+			key: 'TN7BZ-YJKCP-OMTD3-LQKOM-2C5KZ-AWFUQ'
+		});
+		let _this = this;
+		uni.getStorage({
+			key: 'farmId',
+			success: function(res) {
+				_this.farmId = res.data;
+				_this.getFarmDetail();
+				_this.getData();
+				_this.massifFindFarmId();
+				_this.massifFindFarmPests();
+			}
+		});
+	},
+	mounted() {
+		this.loadingData = throttle(this.loadingData, 2000);
+	},
+	methods: {
+		callouttapFunc: function(e) {
+			// uni.navigateTo({
+			// 	url: 'nearTheEquipment?latitude='+this.latitude+'&longitude='+this.longitude+'&farmId='+this.farmId
+			// })
+			uni.navigateTo({
+				url: 'nearTheEquipment?farmId=' + this.farmId
+			});
+		},
+		callPhone(phone) {
+			if (!phone) return;
+			uni.makePhoneCall({
+				// 手机号
+				phoneNumber: phone
+			});
+		},
+		showModel(txt) {
+			this.modelContent = txt;
+			this.popupShow = true;
+		},
+		closePopup() {
+			this.popupShow = false;
+		},
+		onPulling() {},
+		onRefresh() {
+			if (this._freshing) return;
+			this._freshing = true;
+			if (!this.triggered) {
+				//界面下拉触发，triggered可能不是true，要设为true
+				this.triggered = true;
+			}
+			let _this = this;
+			setTimeout(() => {
 				this.triggered = false; //触发onRestore，并关闭刷新图标
 				this._freshing = false;
+				_this.initData();
+			}, 1000);
+		},
+		onRestore() {
+			this.triggered = false; // 需要重置
+			this._freshing = false;
+		},
+		onAbort() {
+			this.triggered = false; //触发onRestore，并关闭刷新图标
+			this._freshing = false;
+		},
+		initData() {
+			this.newsList = [];
+			this.page = 1;
+			this.loadingType = 1;
+			this.contentdown = '';
+			this.getData();
+		},
+		scrolltoupper() {
+			console.info('下拉');
+		},
+		loadingData(e) {
+			this.lastTime = e.timeStamp;
+			if (this.loadingType) {
+				this.page++;
+				this.contentdown = '加载中...';
+				this.getData();
 			}
+		},
+		massifFindFarmId() {
+			//获取农场下所有预警
+			let obj = {
+				pageNum: 1,
+				pageSize: 100,
+				farmId: this.farmId
+			};
+			this.$api.massifFindFarmId(obj).then(res => {
+				this.timeList = res.data.data.massifs;
+				if (this.timeList.length > 0) {
+					this.tabs[2].name = `预警（${this.timeList.length}）`;
+					this.waningTabs[2].name = `环境（${this.timeList.length}）`;
+				}
+			});
 		},
 		massifFindFarmPests() {
 			// 获取农场下所有病虫害
@@ -254,54 +325,80 @@
 				this.pestsList = res.data.data.massifs;
 				if (this.pestsList.length > 0) {
 					this.waningTabs[1].name = `病虫害（${this.pestsList.length}）`;
-					}
-				})
-			},
-			massifFindFarmPests(){ // 获取农场下所有病虫害
-				let obj={
-					pageNum:1,
-					pageSize:100,
-					farmId:this.farmId
 				}
-				this.$api.massifFindFarmPests(obj).then(res=>{
-					this.pestsList=res.data.data.massifs
-					if(this.pestsList.length>0){
-						this.waningTabs[1].name=`病虫害（${this.pestsList.length}）`
+			});
+		},
+		getData() {
+			//获取农场下所有地块
+			let obj = {
+				pageNum: this.page,
+				pageSize: 10,
+				farmId: this.farmId
+			};
+			this.$api.massifSelectFarmId(obj).then(res => {
+				this.newsList = this.newsList.concat(res.data.data.massifs);
+				this.newsList.forEach((item, i) => {
+					if (this.newsList[i].status == 'ONLINE') {
+						this.newsList[i].statusTxt = '在线';
+					} else if (this.newsList[i].status == 'OFFLINE') {
+						this.newsList[i].statusTxt = '离线';
+					} else if (this.newsList[i].status == 'UNACTIVE') {
+						this.newsList[i].statusTxt = '未激活';
+					} else if (this.newsList[i].status == 'DISABLE') {
+						this.newsList[i].statusTxt = '禁用';
 					}
-				})
-			},
-			getData() {//获取农场下所有地块
-				let obj = {
-					pageNum: this.page,
-					pageSize: 10,
+				});
+				if (this.page == 1 && this.newsList.length == 0) {
+					this.loadingType = 0;
+					this.contentdown = '暂无数据';
+				} else if (res.data.data.rowCount == this.newsList.length && this.page == 1 && this.newsList.length < 3) {
+					this.contentdown = '';
+					this.loadingType = 0;
+				} else if (res.data.data.rowCount == this.newsList.length && this.page == 1 && this.newsList.length > 2) {
+					this.contentdown = '无更多数据';
+					this.loadingType = 0;
+				} else if (res.data.data.rowCount == this.newsList.length) {
+					this.loadingType = 0;
+					this.contentdown = '无更多数据';
+				} else {
+					this.contentdown = '上拉加载更多';
+					this.loadingType = 1;
 				}
+			});
 		},
 		getFarmDetail() {
 			this.$api
 				.selectFarmId({
 					farmId: this.farmId
 				})
+				.then(res => {
+					this.farmDetail = res.data.data;
+					this.farmAddress = this.farmDetail.provinceName + this.farmDetail.cityName + this.farmDetail.arerName + (this.farmDetail.address || '');
+					let area = this.farmDetail.provinceName + this.farmDetail.cityName + this.farmDetail.arerName;
 					if (res.data.data.picture) {
-						this.imgArr = res.data.data.picture.split(","); //农场图片
-					} 
-					this.atuoGetLocation(this.farmAddress, area)
-			},
-			tabSelect(e) {
-				this.TabCur = e.currentTarget.dataset.id;
-			},
-			warningTabSelect(e){
-				this.WarningTabCur = e.currentTarget.dataset.id;
-				console.log(this.WarningTabCur == 1)
-			},
-			atuoGetLocation(addr, area) { //根据地址获取经纬度
-				let _this = this
-				qqmapsdk.geocoder({
-					address: addr,
-					complete: res => {
-						if (res.result) {
-							this.latitude = res.result.location.lat
-							this.longitude = res.result.location.lng
-							this.covers = [{
+						this.imgArr = res.data.data.picture.split(','); //农场图片
+					}
+					this.atuoGetLocation(this.farmAddress, area);
+				});
+		},
+		tabSelect(e) {
+			this.TabCur = e.currentTarget.dataset.id;
+		},
+		warningTabSelect(e) {
+			this.WarningTabCur = e.currentTarget.dataset.id;
+			console.log(this.WarningTabCur == 1);
+		},
+		atuoGetLocation(addr, area) {
+			//根据地址获取经纬度
+			let _this = this;
+			qqmapsdk.geocoder({
+				address: addr,
+				complete: res => {
+					if (res.result) {
+						this.latitude = res.result.location.lat;
+						this.longitude = res.result.location.lng;
+						this.covers = [
+							{
 								id: 111,
 								latitude: this.latitude,
 								longitude: this.longitude,
@@ -319,26 +416,31 @@
 									display: 'ALWAYS',
 									textAlign: 'center'
 								}
-					}]
+							}
+						];
+					} else {
+						this.atuoGetLocation(area); //具体位置获取不到经纬度，用省市县地址获取
 					}
-					}
-				});
-			},
-			dispose(row){// 预警处理
-				uni.switchTab({
-					url: '../personal/personal'
-				});
-			},
-			editFarm(){
-				uni.navigateTo({
-					url: 'addMyFarm?id=' + this.farmId
-				})
-			}
+				}
+			});
+		},
+		dispose(row) {
+			// 预警处理
+			uni.switchTab({
+				url: '../personal/personal'
+			});
+		},
+		editFarm() {
+			uni.navigateTo({
+				url: 'addMyFarm?id=' + this.farmId
+			});
 		}
+	}
+};
 </script>
 
 <style lang="scss" scoped>
-	page,
+page,
 	.workOrder {
 		background: #eee;
 
@@ -348,46 +450,171 @@
 		}
 			
 		.list-containers{
-			padding: 10rpx 20rpx;
+			padding: 10rpx 30rpx;
 			padding-top: 100rpx;
+		}
+		.list-container-1{
+			padding: 10rpx 30rpx;
+			padding-top: 0;
 		}
 	}
 
-	
+	.flex {
+		display: flex;
+	}
 
-.cu-timeline {
-	padding: 30rpx 0;
-}
+	.workOrder {
+		height: 100%;
 
-.imgs-box .cu-avatar.lg {
-	width: 23%;
-	height: 150rpx;
-	margin-right: 6px;
-}
-.cu-nav .cu-items {
-	height: 90rpx;
-	display: inline-block;
-	line-height: 90rpx;
-	padding: 0 20rpx;
-	white-space: nowrap;
-	color: #9a9a9a;
-}
-.cu-navs{
-	height: 90rpx;
-	display: inline-block;
-	line-height: 90rpx;
-	padding:20rpx 30rpx 0 30rpx;
-	white-space: nowrap;
-	color: #9a9a9a;
-}
-.bg-white {
-	background-color: #ffffff;
-}
+		.top {
+			position: fixed;
+			background-color: #fff;
+			width: 100%;
+			z-index: 1;
+		}
+	}
 
-.color-black {
-	color: #000000 !important;
-}
-.cu-btn {
-	border-radius: 10rpx;
-}
+	.box-margin {
+		padding-top: 10px;
+	}
+
+	.loading-more {
+		text-align: center;
+		color: #ddd;
+		padding-bottom: 50rpx;
+		padding-top: 20px;
+	}
+
+	.order-title {
+		line-height: 20px;
+	}
+
+	.small-text {
+		color: #999999;
+		font-size: 13px;
+	}
+
+	.state-box {
+		position: relative;
+		right: -15px;
+		padding: 3px 6px;
+		border-top-left-radius: 15px;
+		border-bottom-left-radius: 15px;
+		background: #49BA89;
+		color: #fff;
+	}
+
+	.item-content-box {
+		width: 30%;
+		text-align: center;
+
+		.item-num {
+			font-size: 18px;
+			font-weight: bold;
+		}
+	}
+
+	.timeline-box {
+		text {
+			color: red;
+		}
+
+	}
+
+	// 地图CSS
+	.map-container {
+		position: relative;
+	}
+
+	.detail-name {
+		width: 75%;
+		border-right: 1px solid #eee;
+	}
+
+	.detail-small-txt {
+		color: #999;
+		font-size: 13px;
+	}
+
+	.detail-img {
+		width: 25%;
+		text-align: center;
+	}
+
+	.detail-img image,
+	.detail-name image {
+		width: 30rpx;
+		height: 30rpx;
+	}
+
+	.detail-box {
+		padding: 20rpx;
+		background: #fff;
+		border-radius: 6px;
+	}
+
+	.color-grey {
+		color: #999;
+	}
+
+	.font-size-16 {
+		font-size: 15px;
+	}
+
+	.map-top-box {
+		position: absolute;
+		top: 0;
+		width: 96%;
+		background: #fff;
+		padding: 10rpx 20rpx;
+		left: 2%;
+		border-radius: 4px;
+	}
+
+	.map-bottom-box {
+		position: absolute;
+		bottom: 15rpx;
+		left: 2%;
+		width: 96%;
+		background: #fff;
+		padding: 10rpx 20rpx;
+		border-radius: 4px;
+	}
+
+	.farm-detail-box {
+		padding: 20rpx 30rpx;
+		background: #fff;
+
+		.detail-small-txt {
+			text-indent: 2em;
+		}
+	}
+
+	.cu-timeline {
+		padding: 30rpx 0;
+	}
+
+	.imgs-box .cu-avatar.lg {
+		width: 23%;
+		height: 150rpx;
+		margin-right: 6px;
+	}
+	.cu-nav .cu-items{
+		height: 90rpx;
+		display: inline-block;
+		line-height: 90rpx;
+		padding: 0 20rpx;
+		white-space: nowrap;
+		color: #9A9A9A;
+	}
+	.bg-white{
+		background-color: #FFFFFF;
+	}
+		
+	.color-black{
+		color: #000000 !important;
+	}
+	.cu-btn{
+		border-radius: 5rpx;
+	}
 </style>
